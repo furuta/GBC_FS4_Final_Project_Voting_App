03c3b4d918e9f08cfa2e314e530d69da2a9dc852
pleja <pleja918@gmail.com>
2019-12-13 22:04:25 -0500

    began summary

--- src/pages/SummaryPage.js
+++ src/pages/SummaryPage.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-export default function SummaryPage() {
-  return <div>SummaryPage</div>;
+export default function SummaryPage({ favoriteCandidate, happyWithProgress, birthday, province, temperature}) {
+  return(
+    <Grid container={true} direction="column">
+      <Grid item={true}>
+        <Typography component="h2">Summary Page</Typography>
+      </Grid>
+      <Box m={1} />
+      <Grid item={true}>
+        <Typography component="p">Birthday</Typography>
+        <Typography component="p">{birthday}</Typography>
+        
+      </Grid>
+      <Box m={2} />
+      <Grid item={true} >
+        <Typography component="p">Province</Typography>
+        <Typography component="p">{province}</Typography>
+        <Box m={2} />
+        
+        
+      </Grid>
+    </Grid>
+  ) ;
 }
